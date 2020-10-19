df17bb2c08cac003fc5e41466518639f5cc19aa0
Vansh Bhatia <vansh.bhatia@mayadata.io>
2020-10-19 11:57:13 +0530

    Migrated to v6, addons included
    Signed-off-by: Vansh Bhatia <vansh.bhatia@mayadata.io>

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { withA11y } from "@storybook/addon-a11y";
+import { addDecorator } from '@storybook/react';
+
+addDecorator(withA11y);
+
+export const parameters = {
+    options: {
+        storySort: (a, b) =>
+          a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+      },
+    viewport: {
+        viewports: INITIAL_VIEWPORTS,
+  },
+};
