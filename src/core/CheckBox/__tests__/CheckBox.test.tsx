2d13d600a0c09f10db76c70c63ab9e2d8a6d9e9c
Hasmik8 <hakobyan.hasmik05@gmail.com>
2020-10-26 18:37:56 +0400

    fix error test
    Signed-off-by: Hasmik8 <hakobyan.hasmik05@gmail.com>

--- src/core/CheckBox/__tests__/CheckBox.test.tsx
+++ src/core/CheckBox/__tests__/CheckBox.test.tsx
@@ -1,7 +1,5 @@
 import { render, cleanup } from '@testing-library/react';
 import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import '@testing-library/jest-dom';
 import { KuberaThemeProvider } from '../../../theme';
 import { CheckBox } from '../CheckBox';
 
