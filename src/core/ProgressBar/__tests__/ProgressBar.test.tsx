81c9cfc05b1fe7210d403334d21016383d0f11c9
Hasmik8 <hakobyan.hasmik05@gmail.com>
2020-10-26 18:32:50 +0400

    fix test error
    Signed-off-by: Hasmik8 <hakobyan.hasmik05@gmail.com>

--- src/core/ProgressBar/__tests__/ProgressBar.test.tsx
+++ src/core/ProgressBar/__tests__/ProgressBar.test.tsx
@@ -1,34 +1,26 @@
-import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom';
 import { ProgressBar } from '../ProgressBar';
+import { screen } from '@testing-library/dom';
 
 afterEach(cleanup);
 jest.useFakeTimers();
 
 describe('ProgressBar renders', () => {
   it('Render', () => {
-    const items = [{ value: 80, color: 'rgb(63, 81, 181)', label: 'Success' }];
-    const { getAllByRole, getByText } = render(
+    // const items = [{ value: 80, color: 'rgb(63, 81, 181)', label: 'Success' }];
+    const { getByText } = render(
       <ProgressBar value={80} color={'red'} label="Success" />
     );
-    const elem = getAllByRole('progressbar');
-    /**
-     * test component
-     */
-    elem.forEach((item, index) => {
-      expect(item).toHaveAttribute(
-        'aria-valuenow',
-        items[index].value.toString()
-      );
-      const el = document.getElementsByClassName('MuiLinearProgress-bar');
-      const style = window.getComputedStyle(el[index]);
-      expect(style.backgroundColor).toBe(items[index].color);
-      const labelElment = getByText(
-        items[index].label + ' : ' + items[index].value
-      );
-      expect(labelElment).toBeInTheDocument();
-    });
+    const elem = screen.getByRole('progressbar');
+    // check value progressbar
+    expect(elem.getAttribute('aria-valuenow')).toBe('80');
+    // check color progressbar
+    const el = document.getElementsByClassName('MuiLinearProgress-bar');
+    const style = window.getComputedStyle(el[0]);
+    expect(style.backgroundColor).toBe('rgb(63, 81, 181)');
+    // check labelElement
+    const labelElment = getByText('Success : 80');
+    expect(labelElment);
   });
 });
