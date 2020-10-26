a1d6d2f0c43f63ef241a0086719af617c8a387c1
armen5 <arm.khachatryan11@gmail.com>
2020-10-26 17:24:55 +0400

    adding test cases
    Signed-off-by: armen5 <arm.khachatryan11@gmail.com>

--- src/core/Search/Search.stories.tsx
+++ src/core/Search/Search.stories.tsx
@@ -7,34 +7,34 @@ storiesOf('Search', module)
   // Litmus Portal
   .add('Litmus Portal', () => (
     <ThemedBackground platform="litmus-portal">
-      <Search placeholder="Search" />
+      <Search placeholder="Search" onChange={(e) => e.target.value} />
     </ThemedBackground>
   ))
 
   // Kubera Chaos
   .add('Kubera Chaos', () => (
     <ThemedBackground platform="kubera-chaos">
-      <Search placeholder="Search" />
+      <Search placeholder="Search" onChange={(e) => e.target.value} />
     </ThemedBackground>
   ))
 
   // Kubera Propel
   .add('Kubera Propel', () => (
     <ThemedBackground platform="kubera-propel">
-      <Search placeholder="Search" />
+      <Search placeholder="Search" onChange={(e) => e.target.value} />
     </ThemedBackground>
   ))
 
   // Kubera Portal
   .add('Kubera Portal', () => (
     <ThemedBackground platform="kubera-portal">
-      <Search placeholder="Search" />
+      <Search placeholder="Search" onChange={(e) => e.target.value} />
     </ThemedBackground>
   ))
 
   // Kubera Core
   .add('Kubera Core', () => (
     <ThemedBackground platform="kubera-core">
-      <Search placeholder="Search" />
+      <Search placeholder="Search" onChange={(e) => e.target.value} />
     </ThemedBackground>
   ));
--- src/core/Search/Search.tsx
+++ src/core/Search/Search.tsx
@@ -4,14 +4,16 @@ import { FormControl, InputAdornment, Input } from '@material-ui/core';
 import { BaseSearchProps } from './base';
 import { useStyles } from './styles';
 
-const Search: React.FC<BaseSearchProps> = ({ placeholder }) => {
+const Search: React.FC<BaseSearchProps> = ({ placeholder, onChange }) => {
   // Styles
   const classes = useStyles();
 
   return (
     <FormControl data-testid="search">
       <Input
+        role="Search"
         className={classes.root}
+        onChange={onChange}
         id="input-with-icon-adornment"
         placeholder={placeholder}
         startAdornment={
