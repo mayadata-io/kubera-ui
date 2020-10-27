0f34d91b5b4fbe40c2d19545bf370b5282736a43
armen5 <arm.khachatryan11@gmail.com>
2020-10-27 14:40:21 +0400

    style change
    Signed-off-by: armen5 <arm.khachatryan11@gmail.com>

--- src/core/Search/styles.ts
+++ src/core/Search/styles.ts
@@ -3,9 +3,14 @@ import { makeStyles, Theme } from '@material-ui/core';
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     width: '10.18rem',
-    color: theme.palette.text.primary,
+    color: theme.palette.text.hint,
     '&:before': {
       borderBottom: `0.0625rem solid ${theme.palette.border.main}`,
+    },
+    '& input': {
+      '&:focus': {
+        color: theme.palette.text.primary,
+      },
     },
   },
 }));
