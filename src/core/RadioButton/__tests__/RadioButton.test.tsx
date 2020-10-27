e1df7f9685706465becd2e777fc4e804dd6d0289
saten16 <gsaten0@gmail.com>
2020-10-27 09:17:35 +0400

    test case modify
    Signed-off-by: saten16 <gsaten0@gmail.com>

--- src/core/RadioButton/__tests__/RadioButton.test.tsx
+++ src/core/RadioButton/__tests__/RadioButton.test.tsx
@@ -1,16 +1,34 @@
-import { render } from '@testing-library/react';
 import React from 'react';
-import { KuberaThemeProvider } from '../../../theme';
 import RadioButton from '../../RadioButton';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import { KuberaThemeProvider } from '../../../theme';
+import { screen } from '@testing-library/dom';
+
+afterEach(cleanup);
+jest.useFakeTimers();
 
 describe('Radio Button Component', () => {
   it('Renders', () => {
-    const { getByText } = render(
+    render(
       <KuberaThemeProvider platform="kubera-chaos">
         <RadioButton>Target cluster</RadioButton>
       </KuberaThemeProvider>
     );
-
-    expect(getByText('Target cluster')).toBeTruthy();
+    //get radio byRole
+    const radio = screen.getByRole('radio');
+    //check type
+    expect(radio).toHaveProperty('type', 'radio');
+    //check value
+    expect(radio).toHaveProperty('value', '');
+    //check checked attribute
+    expect(radio).toHaveProperty('checked', false);
+    //change checked value
+    fireEvent.change(radio, { target: { checked: true } });
+    //check checked changed value
+    expect(radio).toHaveProperty('checked', true);
+    //give text value
+    fireEvent.change(radio, { target: { value: 'radio button text' } });
+    //check given value
+    expect(radio).toHaveProperty('value', 'radio button text');
   });
 });
