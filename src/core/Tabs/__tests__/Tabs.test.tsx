5e287a0ccff7aec736255bd1e3c780efe50aca35
saten16 <gsaten0@gmail.com>
2020-10-28 12:46:24 +0400

    fix test
    Signed-off-by: saten16 <gsaten0@gmail.com>

--- src/core/Tabs/__tests__/Tabs.test.tsx
+++ src/core/Tabs/__tests__/Tabs.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Tabs from '../../Tabs';
+import Tabs from '../Tabs';
 import { render, fireEvent, screen } from '@testing-library/react';
 import { KuberaThemeProvider } from '../../../theme';
 
