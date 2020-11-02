029924fb57f59afa86bc8106e0c0bd19079757e3
arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>
2020-11-02 13:09:43 +0530

    import fixes
    Signed-off-by: arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>

--- src/core/RadioButton/RadioButton.tsx
+++ src/core/RadioButton/RadioButton.tsx
@@ -1,5 +1,4 @@
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Radio from '@material-ui/core/Radio';
+import { FormControlLabel, Radio } from '@material-ui/core';
 import React from 'react';
 import { RadioBaseProps } from './base';
 import useStyles from './styles';
