6a3b77327cd2a4e6427a33bcb8bdbb2b2cbcc614
Ritik Srivastava <ritik.srivastava@mayadata.io>
2020-11-13 20:30:14 +0530

    basic radial chart up for use
    Signed-off-by: Ritik Srivastava <ritik.srivastava@mayadata.io>

---
+++ src/lab/Graphs/RadialChart/testRadialChartData.ts
@@ -0,0 +1,8 @@
+import { RadialChartProps } from './base';
+
+const testRadialChartData: RadialChartProps[] = [
+  { value: 100, lable: 'pass' },
+  { value: 30, lable: 'pending' },
+  { value: 80, lable: 'failed' },
+];
+export { testRadialChartData };
