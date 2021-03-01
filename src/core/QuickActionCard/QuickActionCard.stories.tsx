6b04abeeafde79e28b6c12896ab6c69387195f65
Ritik Srivastava <ritik.srivastava@mayadata.io>
2021-03-01 15:59:52 +0530

    quick action card update, export interface in graphs (#92)
    * quick action card update, export interface in graphs
    * k-ui version update
    
    Signed-off-by: Ritik Srivastava <ritik.srivastava@mayadata.io>

--- src/core/QuickActionCard/QuickActionCard.stories.tsx
+++ src/core/QuickActionCard/QuickActionCard.stories.tsx
@@ -4,11 +4,36 @@ import { ThemedBackground } from '../../utils/storybook';
 import { QuickActionCard } from './QuickActionCard';
 import { quickActionTestData } from './testData';
 
-storiesOf('QuickAction', module).add('Kubera Chaos', () => (
-  <ThemedBackground platform="kubera-chaos" row>
-    <QuickActionCard
-      quickActions={quickActionTestData}
-      title={'Quick Actions'}
-    />
-  </ThemedBackground>
-));
+storiesOf('QuickAction', module)
+  .add('Kubera Chaos', () => (
+    <ThemedBackground platform="kubera-chaos" row>
+      <QuickActionCard
+        quickActions={quickActionTestData}
+        title={'Quick Actions'}
+      />
+    </ThemedBackground>
+  ))
+  .add('Kubera Portal', () => (
+    <ThemedBackground platform="kubera-portal" row>
+      <QuickActionCard
+        quickActions={quickActionTestData}
+        title={'Quick Actions'}
+      />
+    </ThemedBackground>
+  ))
+  .add('Kubera Propel', () => (
+    <ThemedBackground platform="kubera-propel" row>
+      <QuickActionCard
+        quickActions={quickActionTestData}
+        title={'Quick Actions'}
+      />
+    </ThemedBackground>
+  ))
+  .add('Kubera Core', () => (
+    <ThemedBackground platform="kubera-core" row>
+      <QuickActionCard
+        quickActions={quickActionTestData}
+        title={'Quick Actions'}
+      />
+    </ThemedBackground>
+  ));
